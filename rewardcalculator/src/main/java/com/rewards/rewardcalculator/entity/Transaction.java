23414c4ba64785731a01cff5e5f5aec3d1f4a07d
Adithya Choudary <pachavav@sacredheart.edu>
2023-09-25 12:13:33 -0400

    update

--- rewardcalculator/src/main/java/com/rewards/rewardcalculator/entity/Transaction.java
+++ rewardcalculator/src/main/java/com/rewards/rewardcalculator/entity/Transaction.java
@@ -1,13 +1,8 @@
 package com.rewards.rewardcalculator.entity;
 
-import java.sql.Timestamp;
+import jakarta.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "TRANSACTION")
